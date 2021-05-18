24d1422b6787f474b397ddeccd75597c501fd067
Jazzminwe <jasweber@web.de>
2021-05-18 22:01:34 +0200

    imported data srts using permalink

--- code.py
+++ code.py
@@ -5,4 +5,16 @@ import seaborn as sns # make crazy visualizations
 import SciPy as sp # needed for seaborn
 import matplotlib.pyplot as plt # needed for seaborn
 
+
 # import data sets
+
+#train set
+train_path = 'https://github.com/sabrinahanke/Location-opening-restaurants/blob/8ed7de276350bc782ba89c7f32972b449f038edb/train.csv'
+df_train = pd.read_csv(train_path)
+df_train.head()
+
+#test set
+test_path = 'https://github.com/sabrinahanke/Location-opening-restaurants/blob/8ed7de276350bc782ba89c7f32972b449f038edb/test.csv'
+df_test = pd.read_csv(test_path, index_col='Id')
+df_test.head()
+
