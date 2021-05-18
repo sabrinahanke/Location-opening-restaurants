8ed7de276350bc782ba89c7f32972b449f038edb
Jazzminwe <jasweber@web.de>
2021-05-18 21:43:10 +0200

    Adds additional libraries

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 # import packages
 import numpy as np # linear algebra
 import pandas as pd # read csv
+import seaborn as sns # make crazy visualizations
+import SciPy as sp # needed for seaborn
+import matplotlib.pyplot as plt # needed for seaborn
 
 # import data sets
