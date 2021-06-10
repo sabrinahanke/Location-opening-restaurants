f91f55362e260acde95da1c62fac2b40e012a325
Jazzminwe <jasweber@web.de>
2021-06-10 22:02:00 +0200

    Started ML Algo for Revenue Prediction

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
 # import packages
-import numpy # linear algebra
+import numpy as np
 import pandas as pd # read csv
 import seaborn as sns # make crazy visualizations
 import scipy # needed for seaborn
 import matplotlib.pyplot as plt # needed for seaborn
 import IPython as IPy # needed for hab's vergessen und bin zu faul zum googeln
 import sklearn.model_selection as sk
+from sklearn.ensemble import RandomForestRegressor
 
 # import data sets
 
@@ -51,11 +52,13 @@ def juhu():
 
 # calculate basic statistics
 def basic_statistics():
-    '''Gives back basic statistical calculations'''
+    '''
+    Gives back basic statistical calculations
+    '''
     train_data['revenue'].describe()
 
 
-# question-specific statisitcs
+# attribute-specific statisitcs
 # open date
 
 # city
@@ -69,15 +72,21 @@ def basic_statistics():
 
 # revenue
 def revenue_analysis():
+    '''
+    Performs basic statistical analysis on revenue
+    '''
     print('ID of most profitable restarant:', train_data["revenue"].max) 
 
 # visualization
 
 # identify most, least and average profitable restaurant
 
+
+# Machine Learning
+
 # divide train_data set
 def split():
-    train_modified = train_data.drop(labels=['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17', 'P18', 'P19', 'P20', 'P21', 'P22', 'P23', 'P24', 'P25', 'P26', 'P27', 'P28', 'P29', 'P30', 'P31', 'P32', 'P33', 'P34', 'P35', 'P36', 'P37'], axis=1)
+    train_modified = train_data.drop(labels=['City', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17', 'P18', 'P19', 'P20', 'P21', 'P22', 'P23', 'P24', 'P25', 'P26', 'P27', 'P28', 'P29', 'P30', 'P31', 'P32', 'P33', 'P34', 'P35', 'P36', 'P37'], axis=1)
     train, test = sk.train_test_split(train_modified, test_size=0.25, train_size=0.75)
     print(train)
     print(test)
@@ -88,15 +97,45 @@ def where():
     train, test = split()
     train = train.drop(lables=['Open Date','Type'])
     test = test.drop(lables=['Open Date','Type'])
-    # apply machine learning
-    
-
 
 # 2. When to open a restaurant?
 # 3. Which type is the best economic choice?
+    
+# 4. revenue prediciton (Decision Tree)
+def predict():
+    train, test = split()
+    
+    # prepare data 
+    # train
+    train.replace('Big Cities', 1, inplace = True)
+    train.replace('Other', 0, inplace = True)
+    train.replace('IL', 1, inplace = True)
+    train.replace('FC', 0, inplace = True)
+    train['Open Date'] = train['Open Date'].str[:2]
 
+    # test
+    test.replace('Big Cities', 1, inplace = True)
+    test.replace('Other', 0, inplace = True)
+    test.replace('IL', 1, inplace = True)
+    test.replace('FC', 0, inplace = True)
+    test['Open Date'] = test['Open Date'].str[:2]
+
+    # change data type of Open Date (str -> int/float)
+    ''' 
+    train['Open Date'] = train['Open Date'].astype(int)
+    test['Open Date'] = test['Open Date'].astype(int)
+    pd.to_numeric(train['Open Date'], downcast = 'integer')
+    '''
+
+    # apply regression model
+    labels = np.array(train['revenue'])
+    features = np.array(train.drop('revenue', axis = 1).columns)
+
+    rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    rf.fit(features, labels)
+    
 
 def main():
-    split()
+    predict()
 if __name__ == "__main__":
     main()
